44b5e68222e93c2f44f822a627eee903ac82bb1a
Chienping Tsung <jianpingzong@outlook.com>
2025-09-26 16:35:14 +0800

    formula terrain

--- formula_terrain.py
+++ formula_terrain.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def terrain_formula(x, y):
+def formula_terrain(x, y):
     z = 30 - \
         3 * (np.sin(0.1 * x + 0.1 * y) + np.cos(0.1 * x - 0.1 * y)) - \
         3 * np.exp(0.05 * (0.1 * x) ** 2 + 0.03 * (0.1 * y) ** 2)
@@ -9,11 +9,11 @@ def terrain_formula(x, y):
 
 
 if __name__ == '__main__':
-    x = np.linspace(-50, 50)
-    y = np.linspace(-50, 50)
+    x = np.linspace(-50, 50, 101)
+    y = np.linspace(-50, 50, 101)
     x, y = np.meshgrid(x, y)
 
-    z = terrain_formula(x, y)
+    z = formula_terrain(x, y)
 
     terrain = np.stack([x, y, z])
 
